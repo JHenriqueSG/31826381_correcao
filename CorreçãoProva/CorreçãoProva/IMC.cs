62e37a425ef3da43b0f72a0cd1547d4ea79538ba
José Henrique da Silva guimarães <42553922+JHenriqueSG@users.noreply.github.com>
2018-10-16 20:39:13 -0300

    Add files via upload

---
+++ CorreçãoProva/CorreçãoProva/IMC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CorreçãoProva
+{
+    class IMC    // classe atributos privados
+    {
+        private double peso;
+        private double altura;
+
+        public IMC()  // metodo construtor padrão
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+
+        }
+
+        public IMC(double peso, double altura)    // segunda variação usuario passa pesso a altura
+        {
+            this.peso = peso;
+            this.altura = altura;
+
+        }
+
+
+        public double Calcular(double peso, double altura)      //metodo Calcular
+        {
+            return peso / (altura * altura);
+
+        }
+
+        public double classificar(double imc)
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ CorreçãoProva/CorreçãoProva/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace CorreçãoProva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            static int Maior(int a, int b)
+             {
+                if(a > b)
+                    return a;
+                else
+                    return b;
+            }
+
+          
+
+            
+
+            /*
+              1. Implemente o método Maior(a, b) que recebe dois números inteiros como parâmetro
+              e retorna qual deles é o maior.
+             */
+
+            int a,
+                b;
+
+            Console.WriteLine("insira um numero");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("insira o segundo valor");
+            b = Int32.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a + " é maior que " + b);
+            }
+
+            else
+            {
+                Console.WriteLine(b + " é maior que " + a);
+            }
+
+            Console.ReadKey(true);
+
+
+
+
+
+        }
+
+        /*2. O índice de massa corporal (IMC) é uma medida internacional usada para calcular se uma
+          pessoa está no peso ideal. Ele é determinado pela divisão da massa do indivíduo pelo
+          quadrado de sua altura, em que a massa está em quilogramas e a altura em metros.*/
+
+
+
+
+
+
+
+        /*3. Se o resto da divisão de um número inteiro por 2 for zero, então, o número é par, caso
+        contrário, é ímpar. Implemente a função lógica Par(numero) que retorna verdadeiro caso o
+        número passado como parâmetro seja par ou falso caso ele seja ímpar.*/
+
+        static bool Par(int numero)
+        {
+            int resto = numero % 2;
+
+            if (resto == 0)
+                return true;
+
+            else
+                return false;
+
+
+        }
+        static string Par2(int numero)
+        {
+            int resto = numero % 2;
+            if (resto == 0)
+                return "par";
+            else
+                return "ímpar";
+            Console.ReadKey(true);
+
+        }
+
+        /**/
+
+
+
+
+
+    }
+
+
+
+
+}
